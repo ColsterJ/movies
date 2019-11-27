349086bfb81e4511279b9acac2f774f2a2237967
Dave Beshero <dbesher@us.ibm.com>
2019-11-27 10:58:20 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+console.log(axios);
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    axios
+      .get(
+        "https://api.themoviedb.org/3/discover/movie?api_key=368d1a9e8f4c583baa60f54870cda46b"
+      )
+      .then(response => {
+        console.log("this is the good stuff", response);
+        if (response && response.data && response.data.results) {
+          this.setState({ movies: response.data.results });
+        }
+      })
+      .catch(error => {
+        console.log("this is the error", error);
+      });
+
+    this.state = {
+      movies: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.movies.map(movie => {
+            const title = movie.title;
+            return <li key={movie.id}>{title}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
